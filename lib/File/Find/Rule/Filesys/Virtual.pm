9ed012662214b33c6c21e91445ff50334c90ec96
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-05-18 16:51:46 +0000

    this may be the one that sprains my brain
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/File-Find-Rule-Filesys-Virtual@2472 c56e198b-7db7-0310-b3ab-8e406f684e33

---
+++ lib/File/Find/Rule/Filesys/Virtual.pm
@@ -0,0 +1,49 @@
+package File::Find::Rule::Filesys::Virtual;
+use strict;
+use warnings;
+
+
+=head1 NAME
+
+File::Find::Rule::Filesys::Virtual - File::Find::Rule adapted to Filesys::Virtual
+
+=head1 SYNOPSIS
+
+ use File::Find::Rule::Filesys::Virtual;
+
+=head1 DESCRIPTION
+
+
+
+1;
+
+__END__
+
+=cut
+
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 COPYRIGHT
+
+Copyright 2004 Richard Clamp.  All Rights Reserved.
+
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
+
+
+=head1 BUGS
+
+None known.
+
+Bugs should be reported to me via the CPAN RT system.
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=File::Find::Rule::Filesys::Virtual>.
+
+
+=head1 SEE ALSO
+
+L<File::Find::Rule>, L<Filesys::Virtual>, L<Net::DAV::Server>
+
+=cut
